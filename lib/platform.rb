c0c14520eb3cd37038486ecab975ee469b2bb1c6
buyoh <15198247+buyoh@users.noreply.github.com>
2024-09-02 03:02:11 +0900

    Implement PlatformStub

--- lib/platform.rb
+++ lib/platform.rb
@@ -54,3 +54,65 @@ class PlatformImpl
     false
   end
 end
+
+class PlatformStub
+  def initialize(logger)
+    @logger = logger
+
+    @map_ssh_host_to_tcp_hosts = {}
+    @map_tcp_host_to_ssh_host = {}
+
+    @started_ssh_host_to_pid = {}
+    @started_pid_to_ssh_host = {}
+    @pid_counter = 10
+  end
+
+  def add_ssh_host_to_tcp_host_for_test(ssh_host, tcp_ip, tcp_port)
+    tcp_host = [tcp_ip, tcp_port]
+    abort "duplicate tcp_host: #{tcp_host}" if @map_tcp_host_to_ssh_host.key?(tcp_host)
+    @map_ssh_host_to_tcp_hosts[ssh_host] ||= []
+    @map_ssh_host_to_tcp_hosts[ssh_host] << tcp_host
+    @map_tcp_host_to_ssh_host[tcp_host] = ssh_host
+  end
+
+  def wait_tcp_port_is_open(ip, port, pid, _trycount = 10)
+    check_pid_and_tcp_port_is_open(ip, port, pid)
+  end
+
+  def start_child_process_ssh(host, _ssh_config_filepath)
+    tcp_hosts = @map_ssh_host_to_tcp_hosts[host]
+    return false unless tcp_hosts
+
+    pid = @pid_counter
+    @pid_counter += 1
+    @started_ssh_host_to_pid[host] = pid
+    @started_pid_to_ssh_host[pid] = host
+
+    pid
+  end
+
+  def is_alive_child_process(pid)
+    @started_pid_to_ssh_host.key?(pid)
+  end
+
+  def kill_child_process(pid)
+    host = @started_pid_to_ssh_host[pid]
+    return unless host
+
+    @started_pid_to_ssh_host.delete(pid)
+    @started_ssh_host_to_pid.delete(host)
+  end
+
+  def check_pid_and_tcp_port_is_open(ip, port, pid)
+    return false unless is_alive_child_process(pid)
+
+    tcp_host = [ip, port]
+
+    ssh_host = @map_tcp_host_to_ssh_host[tcp_host]
+    return false unless ssh_host
+
+    return false unless @started_ssh_host_to_pid.key?(ssh_host)
+
+    true
+  end
+end
